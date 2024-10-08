6b409d1f5d1f2d6051e1200c2887e4cac632c924
Volok13 <volokhova2017@gmail.com>
2024-10-08 17:21:43 +0300

    lesson1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Xxx from "./components/xxx";
+// import Xxx from "./components/xxx";
 
 function App() {
   return (
     <div>
-        <Xxx place={'house'}/>
-        <Xxx place={'club'}/>
+        {/*<Xxx place={'house'}/>*/}
+        {/*<Xxx place={'club'}/>*/}
     </div>
   );
 }
