b0f7589bbb4a74c79c69c2b936ed47ecd8736fbf
Volok13 <volokhova2017@gmail.com>
2024-10-08 17:18:36 +0300

    lesson1

---
+++ src/components/xxx.tsx
@@ -0,0 +1,15 @@
+// import React, {FC} from 'react';
+// interface IProps {
+//     place: string;
+// }
+//
+// const Xxx:FC<IProps> = ({place}) => {
+//     // let {place} = props;
+//     return (
+//         <div>
+//             xxx components in da {place}
+//         </div>
+//     );
+// };
+//
+// export default Xxx;
